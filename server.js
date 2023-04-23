d1033a545dac511e735c76607ad5f916c2d7999d
brayan-cordova <63522101+brayan-cordova@users.noreply.github.com>
2023-04-22 21:56:17 -0600

    creating routes for user

--- server.js
+++ server.js
@@ -1,36 +1,40 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
+const server = express();
+const routes = require('./routes/routes');
+const cors = require('cors');
 
+// initializing server
 
-const app = express();
 const port = process.env.PORT || 3000;
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
 
 // mongodb connection string
-const CONNECTION_STRING = "mongodb+srv://admin:admin@cluster0.7a5dogw.mongodb.net/test";
-const DATABASE = "test";
-var database;
+mongoose.connect("mongodb://127.0.0.1:27017/mean_crud", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, function checkDB(err) {
+    if (err) {
+        console.log('Not Connected to the database, err:  ' + err);
 
-app.listen(port, () => {
-    console.log('Server running on port ' + port);
-    MongoClient.connect(CONNECTION_STRING, {
-        useNewUrlParser: true
-    }, (err, client) => {
+    } else {
 
-        if (err) {
-            console.log('Not Connected to the database, err:  ' + err);
-        } else {
-            database = client.db(DATABASE);
-            console.log('Connection Successfully, Now Connected to MongoDB to the DataBase: ' + DATABASE);
-        }
-
-    })
+        console.log('Connection Successfully, Now Connected to the DataBase on MongoDB ');
+    }
 });
 
-app.get('/', (req, res) => {
-    res.send("Main")
+// server configuration
+server.use(express.json());
+server.use(routes);
+server.use(cors());
+
+server.listen(port, function check(err) {
+
+    if (err) {
+        console.log('Error on server: ' + err);
+
+    } else {
+
+        console.log('Server running on port ' + port);
+    }
+
 });
